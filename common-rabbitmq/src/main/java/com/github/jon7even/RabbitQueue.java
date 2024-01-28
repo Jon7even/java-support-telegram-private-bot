4ae642b7ab1f72605223ffe0a3cb025b37e2a593
JonSeven <mazahaker5555@yandex.ru>
2024-01-28 15:24:16 +0300

    feat: add new module common rabbitmq

---
+++ common-rabbitmq/src/main/java/com/github/jon7even/RabbitQueue.java
@@ -0,0 +1,12 @@
+package com.github.jon7even;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RabbitQueue {
+    public final String TEXT_MESSAGE_UPDATE = "text_message_update";
+    public final String DOC_MESSAGE_UPDATE = "doc_message_update";
+    public final String PHOTO_MESSAGE_UPDATE = "photo_message_update";
+    public final String AUDIO_MESSAGE_UPDATE = "audio_message_update";
+    public final String ANSWER_MESSAGE = "photo_message_update";
+}
