d28305faa7488f6ea2276a6889b1a8f6d085b5a6
JonSeven <mazahaker5555@yandex.ru>
2024-03-30 23:44:29 +0300

    feat: add competitor

---
+++ node/src/main/java/com/github/jon7even/model/competitor/CompetitorEntity.java
@@ -0,0 +1,32 @@
+package com.github.jon7even.model.competitor;
+
+import com.github.jon7even.model.user.UserEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "competitors", schema = "public")
+public class CompetitorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name_competitor", nullable = false)
+    private String name;
+
+    @Column(name = "created_on", nullable = false)
+    private LocalDateTime created;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "creator_id", nullable = false)
+    @ToString.Exclude
+    private UserEntity creator;
+}
