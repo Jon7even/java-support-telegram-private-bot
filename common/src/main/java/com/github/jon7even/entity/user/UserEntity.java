8cbc1a738e6109b801d0a7426111e87ca6ecf8b1
JonSeven <jonseven@ya.ru>
2024-12-28 16:15:19 +0300

    refactor: [Common] пользователь

--- common/src/main/java/com/github/jon7even/entity/user/UserEntity.java
+++ common/src/main/java/com/github/jon7even/entity/user/UserEntity.java
@@ -1,28 +1,34 @@
 package com.github.jon7even.entity.user;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
-
-import static com.github.jon7even.constants.DataTimePattern.DATE_TIME_FORMATTER;
 
 /**
  * Класс описывающий сущность пользователя
  *
  * @author Jon7even
- * @version 1.0
+ * @version 2.0
  */
+@Data
 @Entity
 @Builder
-@Setter
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "user", schema = "bot")
 public class UserEntity {
+
     @Id
     @SequenceGenerator(name = "UserGenId", sequenceName = "user_seq", allocationSize = 7)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "UserGenId")
@@ -50,36 +56,4 @@ public class UserEntity {
     @Column(name = "auth_on")
     @ColumnDefault(value = "false")
     private Boolean authorization;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof UserEntity that)) return false;
-        return id.equals(that.id)
-                && chatId.equals(that.chatId)
-                && firstName.equals(that.firstName)
-                && lastName.equals(that.lastName)
-                && userName.equals(that.userName)
-                && registeredOn.equals(that.registeredOn)
-                && updatedOn.equals(that.updatedOn)
-                && authorization.equals(that.authorization);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, chatId, firstName, lastName, userName, registeredOn, updatedOn, authorization);
-    }
-
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "id=" + id +
-                ", chatId=" + chatId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", userName='" + userName + '\'' +
-                ", registeredOn=" + registeredOn.format(DATE_TIME_FORMATTER) + '\'' +
-                ", authorization='" + authorization +
-                '}';
-    }
 }
