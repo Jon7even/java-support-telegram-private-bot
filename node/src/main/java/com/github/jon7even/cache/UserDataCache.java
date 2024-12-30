d4060230c1dff01bf354cf1c5dbac23be4608caf
JonSeven <jonseven@ya.ru>
2024-12-30 20:13:32 +0300

    refactor: [Node] удаление ненужных методов в UserDataCache

--- node/src/main/java/com/github/jon7even/cache/UserDataCache.java
+++ node/src/main/java/com/github/jon7even/cache/UserDataCache.java
@@ -1,6 +1,5 @@
 package com.github.jon7even.cache;
 
-import com.github.jon7even.dto.company.CompanyBuildingDto;
 import com.github.jon7even.telegram.BotState;
 
 /**
@@ -14,9 +13,5 @@ public interface UserDataCache {
 
     void setBotStateForCacheUser(Long userId, BotState botState);
 
-    void setCompanyForCacheUser(Long userId, CompanyBuildingDto companyBuildingDto);
-
     BotState getBotStateFromCache(Long userId);
-
-    CompanyBuildingDto getCompanyFromCache(Long userId);
 }
