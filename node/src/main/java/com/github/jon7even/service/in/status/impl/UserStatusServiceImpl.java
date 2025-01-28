949ed6fcfbb6225c6045f34a904279a8c8ade17e
JonSeven <jonseven@ya.ru>
2025-01-28 18:42:15 +0300

    feat: [Node] Реализация сервиса статусов UserStatusServiceImpl

--- node/src/main/java/com/github/jon7even/service/in/status/impl/UserStatusServiceImpl.java
+++ node/src/main/java/com/github/jon7even/service/in/status/impl/UserStatusServiceImpl.java
@@ -1,10 +1,14 @@
 package com.github.jon7even.service.in.status.impl;
 
+import com.github.jon7even.entity.UserStatusEntity;
+import com.github.jon7even.mapper.UserStatusMapper;
+import com.github.jon7even.repository.UserStatusRepository;
 import com.github.jon7even.service.in.status.UserStatusService;
 import com.github.jon7even.telegram.BotState;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Реализация сервиса отвечающего за статус бота для пользователей {@link UserStatusService}.
@@ -17,14 +21,30 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserStatusServiceImpl implements UserStatusService {
 
+    private final UserStatusRepository userStatusRepository;
+
+    private final UserStatusMapper userStatusMapper;
+
     @Override
+    @Transactional
     public void setBotStateForUser(Long chatId, BotState botState) {
-        //TODO
+        log.debug("Присваиваю пользователю c [chatId={}] статус [botState={}]", chatId, botState);
+        UserStatusEntity userStatusForSave = userStatusMapper.toUserStatusEntityFromChatIdAndStatus(chatId, botState);
+        userStatusRepository.save(userStatusForSave);
+        log.debug("Статус [botState={}] присвоен пользователю c [chatId={}]", botState, chatId);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public BotState getBotStateForUser(Long chatId) {
-        //TODO
-        return null;
+        log.debug("Получаю статус пользователя c [chatId={}]", chatId);
+        return userStatusRepository.findById(chatId)
+                .map(UserStatusEntity::getStatus)
+                .orElseGet(() -> {
+                    log.warn("Статус у пользователя c [chatId={}] не найден, присваиваю стандартный", chatId);
+                    UserStatusEntity defaultStatus = userStatusMapper.toDefaultUserStatusEntityFromChatId(chatId);
+                    userStatusRepository.save(defaultStatus);
+                    return BotState.MAIN_START;
+                });
     }
 }
