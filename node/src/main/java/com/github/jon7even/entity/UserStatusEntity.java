0b2cd1d9af6bb61e1a00a963cac836a92159382f
JonSeven <jonseven@ya.ru>
2025-01-28 18:01:39 +0300

    refactor: [Node] переделана сущность UserStatusEntity

--- node/src/main/java/com/github/jon7even/entity/UserStatusEntity.java
+++ node/src/main/java/com/github/jon7even/entity/UserStatusEntity.java
@@ -7,11 +7,10 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /**
  * Класс описывающий статус пользователя.
@@ -22,18 +21,18 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "user_status", schema = "bot")
 public class UserStatusEntity {
 
     @Id
+    @NonNull
     @EqualsAndHashCode.Include
     @Column(name = "chat_id", nullable = false)
-    private Long chatId;
+    private final Long chatId;
 
+    @NonNull
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
-    private BotState status;
+    private final BotState status;
 }
