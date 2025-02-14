4ac6babe6ef7bd6302a504d8d05e4322d93594b2
JonSeven <jonseven@ya.ru>
2025-02-14 21:45:39 +0300

    feat: [Node] в ApiAskClientServiceImpl добавлен ApiAskRequestDto

--- node/src/main/java/com/github/jon7even/service/out/client/impl/ApiAskClientServiceImpl.java
+++ node/src/main/java/com/github/jon7even/service/out/client/impl/ApiAskClientServiceImpl.java
@@ -1,14 +1,12 @@
 package com.github.jon7even.service.out.client.impl;
 
+import com.github.jon7even.dto.external.ApiAskRequestDto;
 import com.github.jon7even.service.out.client.ApiAskClientService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
-
-import java.time.Duration;
 
 /**
  * Реализация сервиса {@link ApiAskClientService}, который отправляет запросы к API нейросетям.
@@ -25,9 +23,14 @@ public class ApiAskClientServiceImpl implements ApiAskClientService {
 
     @Override
     public Mono<String> sendAskAndGetAnswerFromApi(String request, String URI) {
+        ApiAskRequestDto askRequestDto = ApiAskRequestDto.builder()
+                .apiKey("key")
+                .message(request)
+                .build();
+
         return webClient.post()
                 .uri(URI)
-                .bodyValue(request)
+                .bodyValue(askRequestDto)
                 .retrieve()
                 .bodyToMono(String.class)
                 .doOnNext(response -> log.info("Ответ от API: {}", response))
